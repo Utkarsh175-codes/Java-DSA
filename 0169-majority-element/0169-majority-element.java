0ede2d8a3958120f217c5764d4e4753aee07d6b6
Utkarsh <utkarshchunidas@gmail.com>
2026-09-24 23:38:18 +0530

    Time: 2 ms (72.28%), Space: 63.5 MB (5.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -17,6 +17,9 @@ class Solution {
             }
         }
 
+        //Below this we check that our element that is majority is more than n/2 or no but if we do not check this condition then also it is fine because Moore's Algorithm always gives majority element that is more than n/2 times 
+        //If an element occurs more than n/2 times in the entire array, it cannot be completely cancelled
+        
         int count1 = 0;
         for(int i=0;i < nums.length; i++){
             if(nums[i] == element){
